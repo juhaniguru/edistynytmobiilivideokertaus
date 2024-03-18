a1f258cff27016372be20ea04780e583748e047c
juhani.kuru <juhani.kuru@lapinamk.fi>
2024-03-18 19:35:55 +0200

    add navhost

--- app/src/main/java/com/example/androidvideokertaus/MainActivity.kt
+++ app/src/main/java/com/example/androidvideokertaus/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.example.androidvideokertaus
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.androidvideokertaus.ui.theme.AndroidVideoKertausTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             AndroidVideoKertausTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NavHost(navController = navController, startDestination = "landingScreen") {
+                        composable("landingScreen") {
+                            LandingScreen()
+                        }
+
+
+                    }
                 }
             }
         }
@@ -30,10 +42,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, age: Int, callbackFunction : () -> Unit) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello my name is $name and I am $age years old"
     )
+    Button(onClick = { callbackFunction() }) {
+        Text("Click me")
+    }
 }
 
